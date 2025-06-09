5bf0ee9179ce5b418750b8f27b45463142a0dd1d
wojteq18 <279730@student.pwr.edu.pl>
2025-06-09 16:48:28 +0200

    bot zoptmalizowany

--- WSI/Lab/lista_3/minmax/src/bot.rs
+++ WSI/Lab/lista_3/minmax/src/bot.rs
@@ -3,186 +3,151 @@ use rand::Rng;
 use crate::board::{WIN_MOVES, LOSE_MOVES, BEST_FIELD, WORST_FIELDS, SECOND_WORST_FIELDS, SECOND_BEST_FIELDS,
 BEST_FIELD_SCORE, SECOND_BEST_SCORE, SECOND_WORST_SCORE, WORST_SCORE};
 
-struct GameState {
-    my_fields: Vec<u32>,
-    enemy_fields: Vec<u32>,
-    available_fields: Vec<u32>,
-    depth: u32,
-    maximizing_player: bool,
+pub(crate) const ME: u8 = 1;
+pub(crate) const ENEMY: u8 = 2;
+pub(crate) const EMPTY: u8 = 0;
+
+pub fn field_to_coords(field: u32) -> (usize, usize) {
+    if field < 11 || field > 55 {
+        panic!("Nieprawidłowy numer pola: {}", field);
+    }
+    let row  = ((field / 10) - 1) as usize;
+    let col = ((field % 10) - 1) as usize;
+    (row, col)
 }
 
-//funkcja oceny heurytycznej
-fn evaluate_state(my_fields: &Vec<u32>, enemy_fields: &Vec<u32>) -> i32 {
-
-    //Sprawdzamy warunki natychmiastowej wygranej dla nas
-    if check_for_four_completed(my_fields) {
-        return 100000;
-    }
-
-    //Sprawdzamy warunki natychmiastowej przegranej dla przeciwnika
-    if check_for_four_completed(enemy_fields) {
-        return -100000;
-    }
-
-    if check_for_three_completed(my_fields) {
-        return -50000
-    }
-
-    if check_for_three_completed(enemy_fields) {
-        return 50000;
-    }
-
-    let mut score = 0;
-
-    if try_win(my_fields, enemy_fields).is_some() {
-        score += 40000; // Nagroda za możliwość wygranej
-    }
-
-    if try_win(enemy_fields, my_fields).is_some() {
-        score -= 40000; // Kara za możliwość przegranej
-    }
-
-    for line4 in WIN_MOVES.iter() {
-        let my_markers_in_line4 = line4.iter().filter(|f| my_fields.contains(f)).count();
-        let enemy_markers_in_line4 = line4.iter().filter(|f| enemy_fields.contains(f)).count();
-
-        if my_markers_in_line4 == 2 && enemy_markers_in_line4 == 0 {
-            // Dwa moje symbole i dwa puste pola w linii do wygranej
-            score += 7000;
-        }
-        if enemy_markers_in_line4 == 2 && my_markers_in_line4 == 0 {
-            // Dwa symbole przeciwnika i dwa puste pola w linii do jego wygranej
-            score -= 7000;
-        }
-    }
-    
-    for line3 in LOSE_MOVES.iter() {
-        let my_markers_in_line3 = line3.iter().filter(|f| my_fields.contains(f)).count();
-        let enemy_markers_in_line3 = line3.iter().filter(|f| enemy_fields.contains(f)).count();
-
-        if my_markers_in_line3 == 2 && enemy_markers_in_line3 == 0 {
-             let empty_spots_count = line3.iter().filter(|f| !my_fields.contains(f) && !enemy_fields.contains(f)).count();
-             if empty_spots_count == 1 { // Dokładnie jedno puste miejsce do utworzenia trójki
-                score -= 1000;
-             }
-        }
-        if enemy_markers_in_line3 == 2 && my_markers_in_line3 == 0 {
-            let empty_spots_count = line3.iter().filter(|f| !my_fields.contains(f) && !enemy_fields.contains(f)).count();
-            if empty_spots_count == 1 {
-                score += 1000;
+fn get_available_moves(board: &[[u8; 5]; 5]) -> Vec<u32> {
+    let mut moves = Vec::new();
+    for r in 0..5 {
+        for c in 0..5 {
+            if board[r][c] == EMPTY {
+                moves.push(((r + 1) * 10 + (c + 1)) as u32);
             }
         }
     }
+    moves
+}
 
-    // Ocena moich pól
-    let my_worst_fields = my_fields.iter().filter(|&&f| WORST_FIELDS.contains(&f)).count();
-    let my_second_worst_fields = my_fields.iter().filter(|&&f| SECOND_WORST_FIELDS.contains(&f)).count();
-    let my_second_best_fields = my_fields.iter().filter(|&&f| SECOND_BEST_FIELDS.contains(&f)).count();
+//funkcja oceny heurytycznej
+fn evaluate_state(board: &[[u8; 5]; 5]) -> i32 {
+    let mut score = 0;
 
-    if my_fields.contains(&BEST_FIELD.clone()) {
-        score += BEST_FIELD_SCORE;
+    // Ocena gróźb wygranej
+    if try_win(board, ME).is_some() { score += 40000; }
+    if try_win(board, ENEMY).is_some() { score -= 40000; }
+    
+    // Ocena par w liniach do wygranej (2 w linii na 4)
+    for line4 in WIN_MOVES.iter() {
+        let mut my_markers = 0;
+        let mut enemy_markers = 0;
+        for &field in line4.iter() {
+            let (r, c) = field_to_coords(field);
+            if board[r][c] == ME { my_markers += 1; }
+            else if board[r][c] == ENEMY { enemy_markers += 1; }
+        }
+        if my_markers == 2 && enemy_markers == 0 { score += 7000; }
+        if enemy_markers == 2 && my_markers == 0 { score -= 7000; }
     }
-    score += (my_worst_fields as i32) * WORST_SCORE;
-    score += (my_second_worst_fields as i32) * SECOND_WORST_SCORE;
-    score += (my_second_best_fields as i32) * SECOND_BEST_SCORE;
 
-    // Ocena pól przeciwnika
-    let enemy_worst_fields = enemy_fields.iter().filter(|&&f| WORST_FIELDS.contains(&f)).count();
-    let enemy_second_worst_fields = enemy_fields.iter().filter(|&&f| SECOND_WORST_FIELDS.contains(&f)).count();
-    let enemy_second_best_fields = enemy_fields.iter().filter(|&&f| SECOND_BEST_FIELDS.contains(&f)).count();
-    if enemy_fields.contains(&BEST_FIELD.clone()) {
-        score -= BEST_FIELD_SCORE;
+    // Ocena groźby przegranej (2 w linii na 3)
+    for line3 in LOSE_MOVES.iter() {
+        let mut my_markers = 0;
+        let mut enemy_markers = 0;
+        for &field in line3.iter() {
+            let (r, c) = field_to_coords(field);
+            if board[r][c] == ME { my_markers += 1; }
+            else if board[r][c] == ENEMY { enemy_markers += 1; }
+        }
+        if my_markers == 2 && enemy_markers == 0 { score -= 1000; } // Kara za groźbę samobója
+        if enemy_markers == 2 && my_markers == 0 { score += 1000; } // Nagroda za groźbę samobója przeciwnika
     }
-    score -= (enemy_worst_fields as i32) * WORST_SCORE;
-    score -= (enemy_second_worst_fields as i32) * SECOND_WORST_SCORE;
-    score -= (enemy_second_best_fields as i32) * SECOND_BEST_SCORE;
+
+    // Ocena pozycyjna
+    for r in 0..5 {
+        for c in 0..5 {
+            let field_val = ((r + 1) * 10 + (c + 1)) as u32;
+            let field_score = if BEST_FIELD == field_val { BEST_FIELD_SCORE }
+                              else if SECOND_BEST_FIELDS.contains(&field_val) { SECOND_BEST_SCORE }
+                              else if SECOND_WORST_FIELDS.contains(&field_val) { SECOND_WORST_SCORE }
+                              else if WORST_FIELDS.contains(&field_val) { WORST_SCORE }
+                              else { 0 };
+            
+            if board[r][c] == ME { score += field_score; }
+            else if board[r][c] == ENEMY { score -= field_score; }
+        }
+    }
 
     score
 }
 
-fn minmax(state: &GameState, depth: i32, mut alpha: i32, mut beta: i32) -> (i32, Option<u32>) {
-    if check_for_three_completed(&state.my_fields) {
-        return (-50000, None);
+fn minmax(board: &mut [[u8; 5]; 5], player: u8, depth: i32, mut alpha: i32, mut beta: i32) -> (i32, Option<u32>) {
+    if check_for_four_completed(board, ME) { return (100000, None); } 
+    if check_for_four_completed(board, ENEMY) { return (-100000, None); }
+    if check_for_three_completed(board, ME) { return (-50000, None); }
+    if check_for_three_completed(board, ENEMY) { return (50000, None); }
+
+    let available_moves = get_available_moves(board);
+    if depth == 0 || available_moves.is_empty() {
+        return (evaluate_state(board), None);
     }
 
-    if check_for_three_completed(&state.enemy_fields) {
-        return (50000, None);
-    }
-
-    if check_for_four_completed(&state.my_fields) {
-        return (100000, None);
-    }
-
-    if state.depth == 0 || state.available_fields.is_empty() {
-        return (evaluate_state(&state.my_fields, &state.enemy_fields), None);
-    }
-
-    if check_for_four_completed(&state.enemy_fields) {
-        return (-100000, None);
-    }
-    
     let mut best_move = None;
-    let mut best_value; //na początku moży być to max int albo min int 
-    if state.maximizing_player {
-        best_value = i32::MIN;
-    } else {
-        best_value = i32::MAX;
-    }
+    let opponent = if player == ME { ENEMY } else { ME };
 
-    for &mv in state.available_fields.iter() {
-        let new_state = GameState {
-            my_fields: if state.maximizing_player {
-                let mut fields = state.my_fields.clone();
-                fields.push(mv);
-                fields
-            } else {
-                state.my_fields.clone()
-            },
-            enemy_fields: if !state.maximizing_player {
-                let mut fields = state.enemy_fields.clone();
-                fields.push(mv);
-                fields
-            } else {
-                state.enemy_fields.clone()
-            },
-            available_fields: state.available_fields.iter().filter(|&&x| x != mv).cloned().collect(),
-            depth: state.depth - 1,
-            maximizing_player: !state.maximizing_player,
-        };
-        let (current_value, _) = minmax(&new_state, depth - 1, alpha, beta);
-        
-        if state.maximizing_player {
-            if current_value > best_value {
-                best_value = current_value;
+    if player == ME { // Tura gracza MAXymalizującego
+        let mut max_eval = i32::MIN;
+        for mv in available_moves {
+            let (r, c) = field_to_coords(mv);
+            
+            board[r][c] = player; // Zrób ruch
+            let (eval, _) = minmax(board, opponent, depth - 1, alpha, beta);
+            board[r][c] = EMPTY;  // Cofnij ruch!
+
+            if eval > max_eval {
+                max_eval = eval;
                 best_move = Some(mv);
-                alpha = alpha.max(best_value); //najlepsze, dotychczas gwarantowane minimum
             }
-        } else {
-            if current_value < best_value {
-                best_value = current_value;
-                best_move = Some(mv);
-                beta = beta.min(best_value); //najlepsze, dotychczas gwarantowane maksimum
+            alpha = alpha.max(eval);
+            if beta <= alpha {
+                break;
             }
         }
-        if beta <= alpha {
-            break; //przycinanie alfa-beta
-        }
-    }
+        (max_eval, best_move)
+    } else { // Tura gracza MINimalizującego
+        let mut min_eval = i32::MAX;
+        for mv in available_moves {
+            let (r, c) = field_to_coords(mv);
 
-    return (best_value, best_move);
+            board[r][c] = player; 
+            let (eval, _) = minmax(board, opponent, depth - 1, alpha, beta);
+            board[r][c] = EMPTY;  
+
+            if eval < min_eval {
+                min_eval = eval;
+                best_move = Some(mv);
+            }
+            beta = beta.min(eval);
+            if beta <= alpha {
+                break;
+            }
+        }
+        (min_eval, best_move)
+    }
 }
 
-pub fn choose_best_move(my_fields: &Vec<u32>, enemy_fields: &Vec<u32>, available_fields: &Vec<u32>, depth: u32) -> Option<u32> {
-    let game_state = GameState {
-        my_fields: my_fields.clone(),
-        enemy_fields: enemy_fields.clone(),
-        available_fields: available_fields.clone(),
-        depth,
-        maximizing_player: true, // Zakładamy, że to my gramy
-    };
+pub fn choose_best_move(my_fields: &Vec<u32>, enemy_fields: &Vec<u32>, depth: u32) -> Option<u32> {
+    let mut board = [[EMPTY; 5]; 5];
+    for &field in my_fields.iter() {
+        let (r, c) = field_to_coords(field);
+        board[r][c] = ME;
+    }
+    for &field in enemy_fields.iter() {
+        let (r, c) = field_to_coords(field);
+        board[r][c] = ENEMY;
+    }
 
-    let (_best_value, best_move) = minmax(&game_state, depth as i32, i32::MIN, i32::MAX);
-    return best_move; 
+    let (_score, best_move) = minmax(&mut board, ME, depth as i32, i32::MIN, i32::MAX);
+    best_move
 }
 
 
@@ -214,65 +179,70 @@ pub fn random_field(fields: &Vec<u32>) -> Option<u32> {
     }
 }
 
-pub fn try_win(my_fields: &Vec<u32>, enemy_fields: &Vec<u32>) -> Option<u32> {
+pub fn try_win(board: &[[u8; 5]; 5], player: u8) -> Option<u32> {
+    let opponent = if player == ME { ENEMY } else { ME };
     for win_line in WIN_MOVES.iter() {
         let mut count = 0;
         let mut empty_field = None;
         let mut possible = true;
         for &field in win_line.iter() {
-            if my_fields.contains(&field) {
+            let (r, c) = field_to_coords(field);
+            if board[r][c] == player {
                 count += 1;
-            } else if enemy_fields.contains(&field) {
+            } else if board[r][c] == opponent {
                 possible = false;
                 break;
-            } else {
+            } else { // Puste pole
                 if empty_field.is_none() {
                     empty_field = Some(field);
-                } else {
+                } else { // Więcej niż jedno puste pole
                     possible = false;
                     break;
                 }
             }
         }
         if possible && count == 3 && empty_field.is_some() {
-            return empty_field; // Zwracamy pole, które można zająć, aby wygrać
+            return empty_field;
         }
     }
-    return None;
+    None
 }
 
-fn check_for_four_completed(player_fields: &Vec<u32>) -> bool {
+fn check_for_four_completed(board: &[[u8; 5]; 5], player: u8) -> bool {
     for win_line in WIN_MOVES.iter() {
-        if win_line.iter().all(|&x| player_fields.contains(&x)) {
-            return true; 
-        }
-    }
-    return false;
-}
-
-fn check_for_three_completed(player_fields: &Vec<u32>) -> bool {
-    for loose_line in LOSE_MOVES.iter() {
-        if loose_line.iter().all(|&x| player_fields.contains(&x)) {
-            return true; 
-        }
-    }
-    return false;
-}
-
-//fn check_for_four_completed(pl)
-pub fn check_lose(my_fields: &Vec<u32>, player_move: u32) -> bool {
-    for lose_move in LOSE_MOVES.iter() {
-        let mut count = 0;
-        for line in lose_move.iter() {
-            if my_fields.contains(line) || *line == player_move {
-                count += 1;
-            }
-        }
-        if count == 3 {
-            return true; // Przeciwnik może wygrać w następnym ruchu
+        if win_line.iter().all(|&field| {
+            let (r, c) = field_to_coords(field);
+            board[r][c] == player
+        }) {
+            return true;
         }
     }
     false
+}
+
+fn check_for_three_completed(board: &[[u8; 5]; 5], player: u8) -> bool {
+    for lose_line in LOSE_MOVES.iter() {
+        if lose_line.iter().all(|&field| {
+            let (r, c) = field_to_coords(field);
+            board[r][c] == player
+        }) {
+            return true;
+        }
+    }
+    false
+}
+
+//fn check_for_four_completed(pl)
+pub fn check_lose(board: &[[u8; 5]; 5], mv: u32, player: u8) -> bool {
+    let (r, c) = field_to_coords(mv);
+    if board[r][c] != EMPTY {
+        return false; 
+    }
+
+    let mut temp_board = *board;
+    temp_board[r][c] = player;
+
+    check_for_three_completed(&temp_board, player)
 }
 
 
--- WSI/Lab/lista_3/minmax/src/main.rs
+++ WSI/Lab/lista_3/minmax/src/main.rs
@@ -1,19 +1,20 @@
+
 mod board;
 mod bot;
 
-use std::io::{Read, Write}; //odczyt i zapis w gnieździe sieciowym
+use std::io::{Read, Write};
 use std::net::TcpStream;
 use board::{set_board, set_move, print_board};
-use bot::{all_fields, delete_field, random_field, check_lose, choose_best_move, try_win};
+use bot::{all_fields, delete_field, random_field, check_lose, choose_best_move, try_win, ME, ENEMY, EMPTY, field_to_coords};
 
 fn main() -> std::io::Result<()> {
-    let mut all_fields = all_fields(); // Inicjalizacja wszystkich pól
+    let mut all_fields = all_fields();
     let mut my_fields: Vec<u32> = Vec::new();
     let mut enemy_fields: Vec<u32> = Vec::new();
 
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 6 {
-        eprintln!("Użycie: {} <nazwa_użytkownika>", args[0]);
+        eprintln!("Użycie: {} <ip> <port> <nr_gracza> <nazwa> <głębokość>", args[0]);
         std::process::exit(1);
     }
     let ip_adress = args[1].clone();
@@ -25,16 +26,14 @@ fn main() -> std::io::Result<()> {
     let player_number: usize = player_number_str.parse().expect("Numer gracza musi byc liczba calkowita!");
     let final_adress = format!("{}:{}", ip_adress, port);
     
-    set_board(); // Inicjalizacja planszy
+    set_board();
     let mut end_game = false;
-
-
 
     // Łączenie z serwerem
     let mut stream = TcpStream::connect(final_adress)?;
     println!("Połączono z serwerem");
 
-    //Wysyałanie identyfikacji
+    // Wysyłanie identyfikacji
     let mut buffer = [0; 16];
     let bytes_read = stream.read(&mut buffer)?;
     let server_msg = String::from_utf8_lossy(&buffer[..bytes_read]);
@@ -44,35 +43,30 @@ fn main() -> std::io::Result<()> {
         stream.flush()?;
     }
 
-
-
     while !end_game {
+
         let mut buffer = [0; 16];
         let bytes_read = match stream.read(&mut buffer) {
-            Ok(0) => {
-                println!("Serwer zakończył połączenie.");
-                break;
-            }
+            Ok(0) => { println!("Serwer zakończył połączenie."); break; }
             Ok(n) => n,
-            Err(e) => {
-                eprintln!("Błąd odczytu z serwera: {}", e);
-                break;
-            }
+            Err(e) => { eprintln!("Błąd odczytu z serwera: {}", e); break; }
         };
 
-        // Konwertuj tylko odczytane bajty na string, usuwając puste bajty (null bytes)
-        let server_msg = String::from_utf8_lossy(&buffer[..bytes_read]); //konwertuje format bajtów na string, usuwa niepotrzebne znaki
+        let server_msg = String::from_utf8_lossy(&buffer[..bytes_read]);
         let server_msg = server_msg.trim_matches('\u{0}').trim();
+
 
         if server_msg.is_empty() {
             continue;
         }
 
         println!("Serwer: {}", server_msg);
-        //parsowanie wiadomości od serwera
         let server_usize: usize = server_msg.parse().unwrap_or(0);
-        enemy_fields.push(server_usize as u32);
-        delete_field(&mut all_fields, server_usize as u32);
+        if server_usize != 0 && server_usize != 600 {
+            enemy_fields.push(server_usize as u32);
+            delete_field(&mut all_fields, server_usize as u32);
+        }
+
 
         let num: i32 = server_msg.parse().unwrap_or(-1);
         let move_code = num % 100;
@@ -84,40 +78,66 @@ fn main() -> std::io::Result<()> {
         }
 
         if msg_code == 0 || msg_code == 6 {
-            if let Some(win_move) = try_win(&my_fields, &enemy_fields) {
+
+            let mut board = [[EMPTY; 5]; 5];
+            for &field in my_fields.iter() {
+                let (r, c) = field_to_coords(field);
+                board[r][c] = ME;
+            }
+            for &field in enemy_fields.iter() {
+                let (r, c) = field_to_coords(field);
+                board[r][c] = ENEMY;
+            }
+
+            if my_fields.len() + enemy_fields.len() == 0 {
+                my_fields.push(33);
+                delete_field(&mut all_fields, 33);
+                set_move(33, player_number);
+                print_board();
+                stream.write_all("33".as_bytes())?;
+                stream.flush()?;
+                continue;
+            }
+
+            
+
+            if let Some(win_move) = try_win(&board, ME) {
+
                 my_fields.push(win_move);
                 delete_field(&mut all_fields, win_move);
                 set_move(win_move as usize, player_number);
                 print_board();
                 stream.write_all(win_move.to_string().as_bytes())?;
                 stream.flush()?;
-                continue; // Jeśli udało się wygrać, kontynuuj
+                continue;
             }
 
-            if let Some(block_move) = try_win(&enemy_fields, &my_fields) {
+            if let Some(block_move) = try_win(&board, ENEMY) {
                 my_fields.push(block_move);
                 delete_field(&mut all_fields, block_move);
                 set_move(block_move as usize, player_number);
                 print_board();
                 stream.write_all(block_move.to_string().as_bytes())?;
                 stream.flush()?;
-                continue; // Jeśli udało się zablokować ruch przeciwnika, kontynuuj
+                continue;
             }
-            // Jeśli nie ma ruchu wygrywającego, wybierz najlepszy ruch
-            match choose_best_move(&my_fields, &enemy_fields, &all_fields, deepness.parse::<u32>().unwrap_or(2)) {
-                Some(win_move) => {
-                    my_fields.push(win_move);
-                    delete_field(&mut all_fields, win_move);
-                    set_move(win_move as usize, player_number);
+            
+            match choose_best_move(&my_fields, &enemy_fields, deepness.parse::<u32>().unwrap_or(3)) {
+                Some(best_move) => {
+                    my_fields.push(best_move);
+                    delete_field(&mut all_fields, best_move);
+                    set_move(best_move as usize, player_number);
                     print_board();
-                    stream.write_all(win_move.to_string().as_bytes())?;
+                    stream.write_all(best_move.to_string().as_bytes())?;
                     stream.flush()?;
                 }
-                None => {
+                None => { 
                     loop {
                         let random = random_field(&all_fields).unwrap_or(0);
-                        if check_lose(&my_fields, random) {
-                            continue;
+                        if random == 0 { println!("Brak ruchów!"); break; }
+                        
+                        if check_lose(&board, random, ME) {
+                            continue; 
                         } else {
                             my_fields.push(random);
                             delete_field(&mut all_fields, random);
